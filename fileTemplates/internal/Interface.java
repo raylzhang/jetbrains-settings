7285c59bd7b0438111b26f96d87819d6dd1bbef6
raylzhang <raylzhang@foxmail.com>
2019-06-12 16:00:58 +0800

    IU-2019.1.3 <ray@zhangleideMacBook-Pro.local Create baseRefactoring.xml, ide.general.xml, project.default.xml, diff.xml, Interface.java, lombok-plugin.xml, Default.xml, find.xml, ui.lnf.xml, filetypes.xml, colors.scheme.xml, _@user_Darcula.icls, path.macros.xml, mavenVersion.xml, templates.xml, Default.xml, Class.java, jdk.table.xml, ray.xml, laf.xml, Mac OS X 10_5_ copy.xml, proxy.settings.xml, vcs.xml, editor.codeinsight.xml, grepConsole.xml, toStringTemplates.xml, keymap.xml, mybatis.xml, editor.xml, debugger.xml, updates.xml

---
+++ fileTemplates/internal/Interface.java
@@ -0,0 +1,12 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+/**
+  * ${NAME}
+  * @Project ${PROJECT_NAME}
+  * @Description TODO
+  * @Author Ray
+  * @Create ${DATE} ${TIME}
+  * @Version 1.0
+  */
+public interface ${NAME} {
+}
